41780fb0b1f603e2cb2a122c67fa4a8c031a77db
xiaowang <psilyup@163.com>
2025-04-23 15:26:05 +0800

    Update DBNoWatermark.js
    直接定位testid 添加和下载

--- DBNoWatermark.js
+++ DBNoWatermark.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        豆包下载无水印
 // @namespace    https://github.com/xiaowang96-github/doubaoNowatermark
-// @version      1.31
+// @version      1.32
 // @description  尝试从豆包下载不带水印的大图
 // @author       xiaowang
 // @match        *://www.doubao.com/*
@@ -23,7 +23,7 @@
             if (!container) {
                 return;
             }
-            const targetDiv = container.querySelector('.right-dVuO4Z');
+            const targetDiv = container.querySelector('.right-HSiTKj');
             if (targetDiv) {
                 const button = targetDiv.querySelector('#download_no_watermark_button');
                 if (button) {
@@ -57,21 +57,21 @@
             }
         });
 
-        observerCanvasWrapper.observe(document.body, { 
-            childList: true, 
+        observerCanvasWrapper.observe(document.body, {
+            childList: true,
             subtree: true,
             attributes: true,
             attributeFilter: ['data-visible']
         });
 
-      
+
         const observerCloseButton = new MutationObserver((mutations) => {
             for (const mut of mutations) {
                 if (mut.type === 'childList') {
                     const closeButton = document.querySelector('[data-testid="edit_image_close_button"]');
                     if (closeButton) {
                         closeButton.addEventListener('click', () => {
-             
+
                             const canvasWrappers =  document.querySelectorAll('div[data-testid="canvas_panel_container"]');
                             canvasWrappers.forEach(canvasWrapper => {
                                 delete canvasWrapper.dataset.processed;
@@ -87,7 +87,7 @@
 
         document.addEventListener('click', function (event) {
             if (event.target && event.target.closest('#download_no_watermark_button')) {
-                const imageContainer = document.querySelector('div[class="center-content-_0Bhud"]');
+                const imageContainer = document.querySelector('div[data-testid="canvas_panel_container"][data-visible="true"]');
                 if (!imageContainer) {
                     return;
                 }
@@ -127,26 +127,26 @@
                 console.error('传入的 container 不存在');
                 return;
             }
-    
+
             const MAX_RETRIES = 5; // 最大重试次数
             const RETRY_INTERVAL = 500; // 重试间隔，单位：毫秒
             let retries = 0;
-        
+
             function tryFindTargetDiv() {
                 // 找到 .right-dVuO4Z 元素
-                const targetDiv = container.querySelector('.right-dVuO4Z');
+                const targetDiv = container.querySelector('.right-HSiTKj');
                 if (targetDiv) {
                     // 检查目标父元素是否还在文档中
                     if (!document.body.contains(targetDiv)) {
                         console.error('目标父元素已不在文档中');
                         return;
                     }
-        
+
                     // 创建新的下载无水印图按钮
                     const newButton = document.createElement('div');
                     newButton.className = 'right-label-btn-wrapper-ryo3eC hover-BfK9He';
                     newButton.id = 'download_no_watermark_button';
-        
+
                     // 创建 SVG 图标
                     const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
                     svgIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
@@ -154,28 +154,28 @@
                     svgIcon.setAttribute("height", "1em");
                     svgIcon.setAttribute("fill", "none");
                     svgIcon.setAttribute("viewBox", "0 0 24 24");
-        
+
                     // 创建 SVG 路径
                     const pathIcon = document.createElementNS("http://www.w3.org/2000/svg", "path");
                     pathIcon.setAttribute("fill", "currentColor");
                     pathIcon.setAttribute("d", "M20.207 12.707a1 1 0 0 0-1.414-1.414L13 17.086V2.5a1 1 0 1 0-2 0v14.586l-5.793-5.793a1 1 0 0 0-1.414 1.414l7.5 7.5c.195.195.45.293.706.293H4a1 1 0 1 0 0 2h16a1 1 0 1 0 0-2h-7.999a1 1 0 0 0 .706-.293z");
-        
+
                     // 将路径添加到 SVG 中
                     svgIcon.appendChild(pathIcon);
-        
+
                     // 将 SVG 添加到按钮中
                     newButton.appendChild(svgIcon);
-        
+
                     // 创建按钮文本标签
                     const spanLabel = document.createElement('span');
                     spanLabel.className = 'btn-label-mw0QhY';
                     spanLabel.textContent = '下载无水印图';
                     // 将文本标签添加到按钮中
                     newButton.appendChild(spanLabel);
-        
+
                     // 将新按钮添加到文档中
                     targetDiv.appendChild(newButton);
-        
+
                     console.log('下载无水印图按钮已添加');
                 } else if (retries < MAX_RETRIES) {
                     retries++;
@@ -184,7 +184,7 @@
                     console.error('经过多次尝试，仍未找到具有 right-dVuO4Z 类名的元素');
                 }
             }
-        
+
             tryFindTargetDiv();
         } catch (error) {
             console.error('addDownloadNoWatermarkButton 函数出错:', error);
